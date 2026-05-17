7faf00948fdd348db92c718b5aed083004c460aa
Hailey <72575893+HaileyLau@users.noreply.github.com>
2026-05-16 19:10:31 -0700

    Reformatted background, redirected buttons and links, and added symptom questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
 import duck from "./assets/chick.png";
+import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 function LandingPage() {
   const navigate = useNavigate();
 
   return (
-    <main className="page">
+    <main className="start-page">
       <section className="card">
         <div className="title-row">
           <h1>NestTogether</h1>
@@ -16,13 +18,16 @@ function LandingPage() {
         <p className="tagline">Stick, Learn, and Grow Together</p>
 
         <button
-          className="role-button selected"
+          className="button"
           onClick={() => navigate("/mother")}
         >
           Expectant Mother
         </button>
 
-        <button className="role-button">
+        <button
+          className="button"
+          onClick={() => navigate("/supporter")}
+        >
           Supporter of<br />
           Expectant Mother
         </button>
@@ -32,8 +37,21 @@ function LandingPage() {
 }
 
 function MotherPage() {
+  const [inputs,setInputs] = useState({});
+  
+  const handleChange = (e)=>{
+    const target = e.target;
+    const value = target.type === 'checkbox'?target.checed :target.value;
+    const name = target.name;
+    setInputs(values => ({...values, [name]:value}))
+  }
+
+  const handleSubmit = (event)=>{
+    //implement submit logic
+  }
+
   return (
-    <main className="mother-page">
+    <main className="page">
       <nav className="navbar">
         <div className="brand">
           <img src={duck} alt="Duck" />
@@ -41,9 +59,95 @@ function MotherPage() {
         </div>
 
         <div className="nav-links">
-          <a>Home</a>
+          <Link to="/">Home</Link>
           <a className="active">Symptoms</a>
-          <a>Resources</a>
+          <Link to="/mother-resources">Resources</Link>
+        </div>
+      </nav><section className="symptoms-section">
+        <h2>Symptoms Survey</h2>
+        <p>Report symptoms you have experienced during pregnancy.</p>
+
+        <div className="questions">
+          <span>Question:</span>
+          <span><input type = "checkbox" checked></input></span>
+          <span>No</span>
+          <div className = "q1"> 
+          <form onSubmit = {handleSubmit}>
+            <label>1. Are you ?
+              <input type = "checkbox" name = "q1" value = {inputs.q1} onChange ={handleChange}/>
+            </label>
+          </form>
+          </div>
+
+          <div className="q2">
+            <form onSubmit={handleSubmit}>
+              <label>2. Have you experienced trouble breathing recently?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
+
+          <div className="q3">
+            <form onSubmit={handleSubmit}>
+              <label>3. Have you experienced chest pain or discomfort?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
+
+          <div className="q4">
+            <form onSubmit={handleSubmit}>
+              <label>4. Have you noticed a fast, racing, or irregular heartbeat?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
+
+          <div className="q5">
+            <form onSubmit={handleSubmit}>
+              <label>5. Have you experienced extreme swelling, especially in your legs, feet, or hands?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
+
+          <div className="q6">
+            <form onSubmit={handleSubmit}>
+              <label>6. Have you experienced unusual or sudden weight gain?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
+
+          <div className="q7">
+            <form onSubmit={handleSubmit}>
+              <label>7. Have you experienced unusual or sudden weight gain?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
+        </div>
+
+
+        <button type = "submit">Submit</button>
+      </section>
+    </main>
+  );
+}
+
+function SupporterPage() {
+  return (
+    <main className="page">
+      <nav className="navbar">
+        <div className="brand">
+          <img src={duck} alt="Duck" />
+          <h1>Supporter</h1>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <a className="active">Symptoms</a>
+          <Link to="/supporter-resources">Resources</Link>
         </div>
       </nav>
 
@@ -61,12 +165,63 @@ function MotherPage() {
   );
 }
 
+function SupporterResourcesPage() {
+  return (
+    <main className="page">
+      <nav className="navbar">
+        <div className="brand">
+          <img src={duck} alt="Duck" />
+          <h1>Supporter</h1>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/supporter">Symptoms</Link>
+          <a className="active">Resources</a>
+        </div>
+      </nav>
+
+      <section className="symptoms-section">
+        <h2>Resources</h2>
+        <p>Report symptoms you have experienced during pregnancy.</p>
+      </section>
+    </main>
+  );
+}
+
+function MotherResourcesPage() {
+  return (
+    <main className="page">
+      <nav className="navbar">
+        <div className="brand">
+          <img src={duck} alt="Duck" />
+          <h1>Supporter</h1>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/mother">Symptoms</Link>
+          <a className="active">Resources</a>
+        </div>
+      </nav>
+
+      <section className="symptoms-section">
+        <h2>Resources</h2>
+        <p>Report symptoms you have experienced during pregnancy.</p>
+      </section>
+    </main>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/mother" element={<MotherPage />} />
+        <Route path="/supporter" element={<SupporterPage />} />
+        <Route path="/mother-resources" element={<MotherResourcesPage />} />
+        <Route path="/supporter-resources" element={<SupporterResourcesPage />} />
       </Routes>
     </BrowserRouter>
   );
