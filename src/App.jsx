14a823a37e8da9eb1b939b57f18ff40a850e6b68
eileenhw <hwangem@uci.edu>
2026-05-16 17:07:22 -0700

    testing questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
 import duck from "./assets/chick.png";
+import{useState} from 'react';
 
 function LandingPage() {
   const navigate = useNavigate();
@@ -32,6 +33,19 @@ function LandingPage() {
 }
 
 function MotherPage() {
+  const [inputs,setInputs] = useState({});
+  
+  const handleChange = (e)=>{
+    const target = e.target;
+    const value = target.type === 'checkbox'?target.checed :target.value;
+    const name = target.name;
+    setInputs(values => ({...values, [name]:value}))
+  }
+
+  const handleSubmit = (event)=>{
+    //implement submit logic
+  }
+
   return (
     <main className="mother-page">
       <nav className="navbar">
@@ -51,15 +65,74 @@ function MotherPage() {
         <h2>Symptoms Survey</h2>
         <p>Report symptoms you have experienced during pregnancy.</p>
 
-        <div className="question-row">
+        <div className="questions">
           <span>Question:</span>
-          <span>Yes</span>
+          <span><input type = "checkbox" checked></input></span>
           <span>No</span>
+          <div className = "q1"> 
+          <form onSubmit = {handleSubmit}>
+            <label>1. Are you ?
+              <input type = "checkbox" name = "q1" value = {inputs.q1} onChange ={handleChange}/>
+            </label>
+          </form>
+          </div>
+
+          <div className="q2">
+            <form onSubmit={handleSubmit}>
+              <label>2. Have you experienced trouble breathing recently?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
+
+          <div className="q3">
+            <form onSubmit={handleSubmit}>
+              <label>3. Have you experienced chest pain or discomfort?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
+
+          <div className="q4">
+            <form onSubmit={handleSubmit}>
+              <label>4. Have you noticed a fast, racing, or irregular heartbeat?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
+
+          <div className="q5">
+            <form onSubmit={handleSubmit}>
+              <label>5. Have you experienced extreme swelling, especially in your legs, feet, or hands?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
+
+          <div className="q6">
+            <form onSubmit={handleSubmit}>
+              <label>6. Have you experienced unusual or sudden weight gain?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
+
+          <div className="q7">
+            <form onSubmit={handleSubmit}>
+              <label>7. Have you experienced unusual or sudden weight gain?
+                <input type="checkbox" name="q1" value={inputs.q1} onChange={handleChange} />
+              </label>
+            </form>
+          </div>
         </div>
+
+
+        <button type = "submit">Submit</button>
       </section>
     </main>
   );
 }
+
 
 export default function App() {
   return (
